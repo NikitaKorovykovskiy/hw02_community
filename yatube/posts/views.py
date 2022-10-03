2927ed7a144ec91e38537a00cc1ee98d8e7fad05
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2022-10-03 19:09:30 +0700

    4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,27 +2,24 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
-
-NUM = 10
+NUMBER_POSTS = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:NUM]
-    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:NUMBER_POSTS]
     context = {
         'posts': posts,
-        'title': title,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:NUM]
 
+    posts = Post.objects.filter(group=group).order_by(
+        '-pub_date')[:NUMBER_POSTS]
     context = {
         'group': group,
         'posts': posts,
-        'title': f'Записи сообщества {group}',
     }
     return render(request, 'posts/group_list.html', context)
