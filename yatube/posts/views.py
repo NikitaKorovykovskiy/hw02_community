648edc0aa9a1f3b3ad5837d928075177994269c9
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2022-10-04 19:51:07 +0700

    4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,7 +17,8 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
 
     posts = Post.objects.filter(group=group).order_by(
-        '-pub_date')[:NUMBER_POSTS]
+        '-pub_date'
+    )[:NUMBER_POSTS]
     context = {
         'group': group,
         'posts': posts,
